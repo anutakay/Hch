1cea85b7f67d2e3902a380a8a43cc30d1e6afcbd
A.Kaygorodova <akaygorodova@issart.com>
2020-10-21 06:22:09 +0700

    add navigation component

--- app/src/main/java/ru/anutakay/mypicture/presentation/ui/picture/PictureActivity.kt
+++
@@ -1,9 +0,0 @@
-package ru.anutakay.mypicture.presentation.ui.picture
-
-import androidx.fragment.app.Fragment
-import ru.anutakay.mypicture.presentation.ui.SingleFragmentActivity
-
-class PictureActivity : SingleFragmentActivity(), PictureFragment.InteractionListener {
-
-    override fun createFragment(): Fragment = PictureFragment.newInstance()
-}
