d742b3ac796e2f2235b516e7dd37c6457a86352a
A.Kaygorodova <akaygorodova@issart.com>
2020-10-21 08:18:12 +0700

    add new fragment

--- presentation/editor-presentation/src/main/kotlin/ru/anutakay/mypicture/common/presentation/editor/EditorFragment.kt
+++
@@ -1,6 +0,0 @@
-package ru.anutakay.mypicture.common.presentation.editor
-
-import androidx.fragment.app.Fragment
-
-class EditorFragment : Fragment() {
-}
---
+++ presentation/editor-presentation/src/main/kotlin/ru/anutakay/mypicture/presentation/editor/EditorFragment.kt
@@ -0,0 +1,8 @@
+package ru.anutakay.mypicture.presentation.editor
+
+import ru.anutakay.mypicture.presentation.common.BaseFragment
+
+class EditorFragment : BaseFragment() {
+
+    override fun layoutId(): Int = R.layout.editor
+}
